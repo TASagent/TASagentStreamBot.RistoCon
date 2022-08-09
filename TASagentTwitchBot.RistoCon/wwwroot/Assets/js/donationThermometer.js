4ab7ecdd32df353b9d84fefe8733dae26c4ff30d
TASagent <TASagent.Streams@gmail.com>
2022-08-09 10:42:57 -0700

    Integration donation improvements

--- TASagentTwitchBot.RistoCon/wwwroot/Assets/js/donationThermometer.js
+++ TASagentTwitchBot.RistoCon/wwwroot/Assets/js/donationThermometer.js
@@ -9,18 +9,44 @@ let formatter = new Intl.NumberFormat('en-US', {
   currency: 'USD'
 });
 
-function SetAmount(stateValue) {
-  var percentage = stateValue.currentAmount / 6000.0;
-  percentage = Math.min(Math.max(percentage, 0.0), 1.0);
+let goalAmount = 2000;
+let currentAmount = 0;
 
-  $('#thermometerMask').animate({ height: (100 * (1 - percentage)) + '%' }, 100);
+//Immediately set the state
+function SetState(data) {
+  currentAmount = data.newAmount;
+  goalAmount = data.newGoal;
+  SetBar();
 }
 
-connection.on('SetAmount', SetAmount);
+//Sets the current funding amount
+function UpdateAmount(data) {
+  currentAmount = data.newAmount;
+  AnimateBar();
+}
+
+function AnimateBar() {
+  var percentage = currentAmount / goalAmount;
+  percentage = Math.min(Math.max(percentage, 0.0), 1.0);
+
+  $('#thermometerMask').animate({ height: (100 * (1 - percentage)) + '%' }, 500);
+  $('#thermometerGrowthMask').delay(1000).animate({ height: (100 * (1 - percentage)) + '%' }, 500);
+}
+
+function SetBar() {
+  var percentage = currentAmount / goalAmount;
+  percentage = Math.min(Math.max(percentage, 0.0), 1.0);
+
+  $('#thermometerMask').height(`${(100 * (1 - percentage))}%`);
+  $('#thermometerGrowthMask').height(`${(100 * (1 - percentage))}%`);
+}
+
+connection.on('SetState', SetState);
+connection.on('UpdateAmount', UpdateAmount);
 
 async function Initiate() {
   await connection.start();
-  await connection.invoke("RequestAmount");
+  await connection.invoke('RequestState');
 }
 
 Initiate();
